35f5bdc2187c54ca3230b44f165b9e73cf33d5f4
Diego Barrios Romero <eldruin@gmail.com>
2019-03-12 19:51:46 +0100

    Simplify example

--- examples/std_hasher.rs
+++ examples/std_hasher.rs
@@ -1,16 +1,11 @@
+// Example using the std::hash::Hasher interface.
+
 extern crate wyhash;
+use wyhash::WyHash;
+use std::hash::Hasher;
 
-#[cfg(feature = "std")]
-fn run() {
-    use self::wyhash::WyHash;
-    use std::hash::Hasher;
-
+fn main() {
     let mut hasher = WyHash::with_seed(1);
     hasher.write(&[0]);
     assert_eq!(0xcb4b8ebdf7240e2c, hasher.finish());
-}
-
-fn main() {
-    #[cfg(feature = "std")]
-    run();
 }
