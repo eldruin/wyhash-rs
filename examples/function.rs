30354509c30d0145d161829c97f4d6ca53708d74
Diego Barrios Romero <eldruin@gmail.com>
2019-03-14 18:58:47 +0100

    Add example of using prng function

--- examples/function.rs
+++ examples/function.rs
@@ -1,8 +1,9 @@
-// Example of using the free function.
+// Example of using the free functions.
 
 extern crate wyhash;
-use wyhash::wyhash;
+use wyhash::{wyhash, wyrng};
 
 fn main() {
     assert_eq!(0xcb4b8ebdf7240e2c, wyhash(&[0], 1));
+    assert_eq!(0xd603133dc4196d3, wyrng(1));
 }
