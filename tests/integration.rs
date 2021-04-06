fb138f867a2eb823c87dab6b0bc06e46959a070b
Diego Barrios Romero <eldruin@gmail.com>
2021-04-07 00:46:31 +0200

    Add test for default-constructed final3 version

--- tests/integration.rs
+++ tests/integration.rs
@@ -1,11 +1,18 @@
 extern crate core;
 extern crate wyhash;
 use core::hash::Hasher;
-use wyhash::v1::WyHash as WyHashV1;
+use wyhash::{final3, v1};
 
 #[test]
-fn default_constructed() {
-    let mut hasher = WyHashV1::default();
+fn v1_default_constructed() {
+    let mut hasher = v1::WyHash::default();
     hasher.write(&[0]);
     assert_eq!(0x8c73_a8ab_4659_6ae4, hasher.finish());
+}
+
+#[test]
+fn final3_default_constructed() {
+    let mut hasher = final3::WyHash::default();
+    hasher.write(&[0]);
+    assert_eq!(0x22a2_d5db_3856_770f, hasher.finish());
 }
