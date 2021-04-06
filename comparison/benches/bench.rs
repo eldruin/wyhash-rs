4dcc367f155e5b9ba08164b80639a06962c3ca82
Diego Barrios Romero <eldruin@gmail.com>
2021-04-07 00:22:35 +0200

    Add final3 version to benchmark

--- comparison/benches/bench.rs
+++ comparison/benches/bench.rs
@@ -1,4 +1,4 @@
-use comparison::ffi::c_wyhash;
+use comparison::ffi::{c_wyhash_final3, c_wyhash_v1};
 use core::hash::Hasher;
 use criterion::{criterion_group, criterion_main, BenchmarkId, Criterion, Throughput};
 use fnv::FnvHasher;
@@ -7,7 +7,7 @@ use rand::distributions::Standard;
 use rand::prelude::*;
 pub use std::collections::hash_map::DefaultHasher;
 use twox_hash::XxHash;
-use wyhash::{wyhash, WyHash};
+use wyhash::{final3, v1};
 
 pub fn hasher_bench<H>(data: &[u8])
 where
@@ -24,6 +24,8 @@ fn benchmark(c: &mut Criterion) {
         .iter()
         .map(|&l| (&mut rng).sample_iter(Standard).take(l).collect())
         .collect();
+
+    let secret = final3::make_secret(0);
 
     let mut group = c.benchmark_group("all");
     for input in data.iter() {
@@ -59,27 +61,56 @@ fn benchmark(c: &mut Criterion) {
         );
 
         group.bench_with_input(
-            BenchmarkId::new("wyhash (C function)", length),
+            BenchmarkId::new("wyhash v1 (C function)", length),
             input,
             |b, input| {
                 b.iter(|| unsafe {
-                    c_wyhash(input.as_ptr() as *const libc::c_void, length as u64, 0)
+                    c_wyhash_v1(input.as_ptr() as *const libc::c_void, length as u64, 0)
                 });
             },
         );
 
         group.bench_with_input(
-            BenchmarkId::new("wyhash (Rust function)", length),
+            BenchmarkId::new("wyhash v1 (Rust function)", length),
             input,
             |b, input| {
-                b.iter(|| wyhash(&input, 0));
+                b.iter(|| v1::wyhash(&input, 0));
             },
         );
 
         group.bench_with_input(
-            BenchmarkId::new("wyhash (Rust Hasher)", length),
+            BenchmarkId::new("wyhash v1 (Rust Hasher)", length),
             input,
-            |b, input| b.iter(|| hasher_bench::<WyHash>(input)),
+            |b, input| b.iter(|| hasher_bench::<v1::WyHash>(input)),
+        );
+
+        group.bench_with_input(
+            BenchmarkId::new("wyhash final3 (C function)", length),
+            input,
+            |b, input| {
+                b.iter(|| unsafe {
+                    c_wyhash_final3(
+                        input.as_ptr() as *const libc::c_void,
+                        length as u64,
+                        0,
+                        secret.as_ptr() as *const libc::c_ulonglong,
+                    )
+                });
+            },
+        );
+
+        group.bench_with_input(
+            BenchmarkId::new("wyhash final3 (Rust function)", length),
+            input,
+            |b, input| {
+                b.iter(|| final3::wyhash(&input, 0, secret));
+            },
+        );
+
+        group.bench_with_input(
+            BenchmarkId::new("wyhash final3 (Rust Hasher)", length),
+            input,
+            |b, input| b.iter(|| hasher_bench::<final3::WyHash>(input)),
         );
     }
     group.finish();
